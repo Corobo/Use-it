778082647c0fee26541b8198401f3d2416eb3fd9
Juanfa <juanfpo96@gmail.com>
2016-12-15 16:54:17 +0100

    ProjectoBase

---
+++ src/resource.js
@@ -0,0 +1,35 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png",
+    boton_jugar_png : "res/boton_jugar.png",
+    menu_titulo_png : "res/menu_titulo.png",
+    tiles32_png: "res/tiles32.png",
+    mapa1_tmx: "res/mapa1.tmx",
+    mapa2_tmx: "res/mapa2.tmx",
+    moneda_png : "res/moneda.png",
+    moneda_plist : "res/moneda.plist",
+    playerrunright_png : "res/playerrunright.png",
+    playerrunright_plist : "res/playerrunright.plist",
+    playerjumpright_png : "res/playerjumpright.png",
+    playerjumpright_plist : "res/playerjumpright.plist",
+    playeridleright_png : "res/playeridleright.png",
+    playeridleright_plist : "res/playeridleright.plist",
+    playerdieright_png : "res/playerdieright.png",
+    playerdieright_plist : "res/playerdieright.plist",
+    disparo_png : "res/disparo.png",
+    disparo_plist : "res/disparo.plist",
+    playershootright_png : "res/playershootright.png",
+    playershootright_plist : "res/playershootright.plist",
+    animacion_cuervo_png: "res/animacion_cuervo.png",
+    animacion_cuervo_plist: "res/animacion_cuervo.plist",
+    disparo_png: "res/disparo.png",
+    disparo_plist: "res/disparo.plist",
+    playershootright_png: "res/playershootright.png",
+    playershootright_plist: "res/playershootright.plist",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
